3c4aa6d6bd1072001311d8e9f98b6caec846a64a
hayeknizam <vinicius.andrei@live.com>
2020-05-28 13:36:46 -0300

    Adicionando Comentario

--- exemplo-02.php
+++ exemplo-02.php
@@ -1,6 +1,10 @@
 <?php
  	$anoNascimento = 1999;
  	$nomeCompleto = "";
+ 	// comentario simples de linha, exemplo: na linha de baixo temos uma variavel com numero no nome
  	$nome1 = "João";
  	$nome_1 = "Maria";
+ 	//Adicionando comentario de bloco
+ 	/* É igual no c, ou c++*/
+
 ?>
