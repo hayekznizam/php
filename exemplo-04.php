8deb6e4bfeb2abd6a4844f6dd7927260582dd2c4
hayeknizam <vinicius.andrei@live.com>
2020-05-28 15:16:21 -0300

    Usando variaveis via query e ip do usuario

--- exemplo-04.php
+++ exemplo-04.php
@@ -8,6 +8,15 @@
 	$nome = (int) $_GET["a"];
 	var_dump($nome);
 	echo $nome;
+	///
+	//Pegando ip do usuario
+	$ip = $_SERVER["REMOTE_ADDR"];
+	echo $ip;
+	
+	$ip = $_SERVER["SCRIPT_NAME"];
+	echo $ip;
+	
+
 
 
 ?>
