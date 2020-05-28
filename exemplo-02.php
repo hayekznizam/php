18061138b4b6d2d197a1c77bf232b37d684ad117
hayeknizam <vinicius.andrei@live.com>
2020-05-28 13:47:53 -0300

    Usando is Set

--- exemplo-02.php
+++ exemplo-02.php
@@ -6,5 +6,18 @@
  	$nome_1 = "Maria";
  	//Adicionando comentario de bloco
  	/* É igual no c, ou c++*/
+ 	// essa variavel aqui na frentes nao pode ser usado é restrito $this 
+ 	echo $nome1;
+
+ 	echo "<br/>"; //pulando linha
+
+ 	//unset($nome1); //limpando a variavel na memoria
+	//is set, se ela existe, se nome1 estiver definido escreva nom1
+ 	if(isset($nome1)){ 
+ 		echo $nome1;
+ 	}
+ 	
+ 
+
 
 ?>
