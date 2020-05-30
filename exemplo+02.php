fa15f29ebd4fa16ace920a3f2c9dfe0e1446b771
hayeknizam <vinicius.andrei@live.com>
2020-05-30 15:29:02 -0300

    Adicionado exemplo com operadores e Criando Arquivo Exemplo arquivo exemplo

---
+++ exemplo+02.php
@@ -0,0 +1,5 @@
+<?php
+$valorTotal = 0;
+
+
+?>
--- exemplo-01.php
+++ exemplo-01.php
@@ -1,3 +1,12 @@
 <?php
 //Operadores em PHP
+
+$nome = "Vinícius <br>";
+$sobrenome = "Andrei<br>";
+echo $nome . " mais alguma coisa<br>";
+echo $nome . " ". $sobrenome. "<br/>";
+
+$nome .= " Treinamentos <br>";
+echo $nome;
+
 ?>
