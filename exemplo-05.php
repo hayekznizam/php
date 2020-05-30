6f7bd1859b8562b749f19eaf8477c99dcbd45fa4
hayeknizam <vinicius.andrei@live.com>
2020-05-30 15:55:00 -0300

    Exemplo de SpaceShip

--- exemplo-05.php
+++ exemplo-05.php
@@ -1,5 +1,13 @@
 <?php
 //Operadores SpaceShip
 
+$a = 35;
+$b = 60;
+
+var_dump($a <=> $b); //operador do php 7
+//se o a for maior traz 1, se os dois forem iguais 0, e se b for maior -1
+
+
+
 
 ?>
