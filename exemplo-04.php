3c6c2ec097f3112c36c8a4ad29d889a88e81c548
hayeknizam <vinicius.andrei@live.com>
2020-05-30 15:48:59 -0300

    Criando Exemplo 5 - Space Shift e exemplo de operadores de igualdade

--- exemplo-04.php
+++ exemplo-04.php
@@ -1,7 +1,29 @@
 <?php
-$a = 30;
+$a = 55.0;
 $b =55;
-var_dump($a > $b);
+var_dump($a > $b); 
+echo"<br>";
+
+var_dump($a <$b);
+
+echo"<br>";
+
+var_dump($a == $b);
+
+echo"<br>";
+
+//comparando valor e , igualdade de identidade
+var_dump($a === $b);
+
+echo"<br>";
+
+var_dump($a != $b);
+
+echo"<br>";
+
+//comparando tambem o tipo de dado
+var_dump($a !== $b);
+
 
 
 
---
+++ exemplo-05.php
@@ -0,0 +1,5 @@
+<?php
+//Operadores Space Shift
+
+
+?>
