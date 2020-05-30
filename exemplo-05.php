24b3908251377d0e6cd2c89377137ce71dc3a81b
hayeknizam <vinicius.andrei@live.com>
2020-05-30 15:09:20 -0300

    Criando funcoes com escopo de variáveis

--- exemplo-05.php
+++ exemplo-05.php
@@ -1,4 +1,23 @@
 <?php
 
+$nome = "Vinícius";
+
+function teste(){
+	global $nome;
+	echo $nome;
+	echo "<br/>";
+	
+}
+
+function teste2(){
+	$nome = "João";
+	echo $nome." agora no teste2";
+
+
+}
+
+
+teste();
+teste2();
 
 ?>
