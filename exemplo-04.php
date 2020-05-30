f82cf8669e71b79aa1d05b9effac2d8e7aed5ddb
hayeknizam <vinicius.andrei@live.com>
2020-05-30 14:53:02 -0300

    atualizando arquivo

--- exemplo-04.php
+++ exemplo-04.php
@@ -1,20 +1,24 @@
 <?php
-	//Variáveis pré defiinidas
+	//Variáveis pré defiinidas ou Arrays Super Globais
 
 	$nome = $_GET["a"];
 	var_dump($nome);
 	echo $nome;
+	echo "<br/>";
 	//convertendo o tipo da variavel para int
 	$nome = (int) $_GET["a"];
 	var_dump($nome);
 	echo $nome;
+	echo "<br/>";
 	///
 	//Pegando ip do usuario
 	$ip = $_SERVER["REMOTE_ADDR"];
 	echo $ip;
-	
+	echo "<br/>";
+
 	$ip = $_SERVER["SCRIPT_NAME"];
 	echo $ip;
+	echo "<br/>";
 	
 
 
