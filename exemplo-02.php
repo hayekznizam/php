b1accbca294fdee9b4ffbd8303aa8d128aa006b9
hayeknizam <vinicius.andrei@live.com>
2020-06-01 10:34:19 -0300

    Criado Exemplo 02 - Funções em String - Maiusculo/Minusculo

---
+++ exemplo-02.php
@@ -0,0 +1,28 @@
+<?php
+
+//fun~ções 	
+$nome = "vinícius andrei";
+
+//imprimindo string em maiusculo
+echo $nome;
+echo strtoupper($nome);
+//$nome = strtoupper($nome);
+//imprimindo em minusculo
+echo strtolower($nome);
+
+echo "<br>";
+//Primeiras em Maiusculo
+echo ucwords($nome);
+echo "<br>";
+echo ucfirst($nome);
+
+//
+
+
+
+
+
+
+
+
+?>
