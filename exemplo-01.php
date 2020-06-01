e6718a48b66f3b29efed3b679b5db47c94dd6a4a
hayeknizam <vinicius.andrei@live.com>
2020-06-01 10:27:25 -0300

    Diferença entre aspas duplas e simple

--- exemplo-01.php
+++ exemplo-01.php
@@ -1,4 +1,18 @@
 <?php
+// String - Diferença entre aspas simples e duplas
+// interpolação de variáveis normalmente é um recurso do php, em outras linguagens é necessario concatenar
+
+$nome = "Vinícius";
+$nome2 = 'Treinamentos';
+
+var_dump($nome);
+var_dump($nome2);
+
+echo "ABC $nome";
+echo 'ABC $nome';
+
+
+
 
 ?>
 
