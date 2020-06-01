fa8b59d7c8c0edaeba3376e827b2972d4330ef57
hayeknizam <vinicius.andrei@live.com>
2020-06-01 10:17:12 -0300

    Criado Exemplo 01 - String

--- exemplo-01.php
+++ exemplo-01.php
@@ -1,21 +1,8 @@
 <?php
-<<<<<<< HEAD
-
-
-
-
 
 ?>
-=======
-//Operadores em PHP
 
-$nome = "Vinícius <br>";
-$sobrenome = "Andrei<br>";
-echo $nome . " mais alguma coisa<br>";
-echo $nome . " ". $sobrenome. "<br/>";
 
-$nome .= " Treinamentos <br>";
-echo $nome;
 
-?>
->>>>>>> 570640427ae8caf055d4af1a665c3c0d9f662518
+
+
