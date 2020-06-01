c4f16b389e53a2367b503c52352dd6e5aa25ac4c
hayeknizam <vinicius.andrei@live.com>
2020-06-01 13:24:57 -0300

    Foreach Exemplos

--- for/exemplo-02.php
+++ for/exemplo-02.php
@@ -1,5 +1,5 @@
 <?php
-//Tomar cuidado
+//Tomar cuidado, isso faz um loop infinito
 
 for( $i = 0;$i<10; $i--) {
 	echo $i. "<br>";
---
+++ foreach/exemplo-01.php
@@ -0,0 +1,22 @@
+<?php
+
+//FOREACH - utilizado para percorrer array, para cada item que exista execute
+
+$meses = array(
+	"Janeiro","Fevereiro","Março","Abril",
+	"Maio","Junho","Julho","Agosto",
+	"Setembro","Outubro","Novembro","Dezembro"
+);
+
+foreach ($meses as $mes) {
+	echo "O mês é:" . $mes. "<br>";
+}
+
+foreach ($meses as $index => $mes) {
+	echo "Indice: ".$index."<br>";
+	echo "O mês é:" . $mes. "<br><br>";
+}
+
+
+
+?>
