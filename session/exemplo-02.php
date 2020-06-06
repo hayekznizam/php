29819e58cb67a208a9e399ef9ebc1a11fd6b5edd
hayeknizam <vinicius.andrei@live.com>
2020-06-06 12:00:21 -0300

    Apagando Variavies de session e apagando sessions

--- session/exemplo-02.php
+++ session/exemplo-02.php
@@ -3,7 +3,12 @@
 require_once("config.php");
 //session_start();
 
+//apagando a variavel de sessao
+session_unset($_SESSION['nome']);
+
 echo $_SESSION['nome'];
 
+//apagando a sessao
+session_destroy();
 
 ?>
