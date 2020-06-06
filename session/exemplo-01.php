bf3ceac255a56e28bba1a90d3c65012c648a9f46
hayeknizam <vinicius.andrei@live.com>
2020-06-06 11:55:56 -0300

    Criando e utilizando Sessões

---
+++ session/exemplo-01.php
@@ -0,0 +1,12 @@
+<?php
+require_once("config.php");
+//SESSÕES
+//NAO INICIA AUTOMATICAMENTE NO PHP
+//sessões estao funcionando nessa página
+//session_start();
+$_SESSION["nome"] = "Hcode";
+
+
+
+
+?>
